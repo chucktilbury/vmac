65798d8b57c315f7cf9d7347664c1aa801925896
chuck <chucktilbury@gmail.com>
2026-06-07 09:58:24 -0500

    Update opcodes

--- src/vm_common/opcodes.h
+++ src/vm_common/opcodes.h
@@ -78,24 +78,25 @@ typedef enum _opcode_t {
     REG_IR31 = 0x4F,
     REG_ISP = 0x50,
     REG_IIP = 0x51,
-    REG_IFLAGS = 0x52,
 
     // operand types
-    OPERAND_INT8 = 0x60,
-    OPERAND_INT16 = 0x61,
-    OPERAND_INT32 = 0x62,
-    OPERAND_INT64 = 0x63,
-    OPERAND_UINT8 = 0x64,
-    OPERAND_UINT16 = 0x65,
-    OPERAND_UINT32 = 0x66,
-    OPERAND_UINT64 = 0x67,
-    OPERAND_ARRAY = 0x68,
-    OPERAND_FLOAT = 0x69,
-    OPERAND_STRING = 0x6A,
-    OPERAND_REG = 0x6B,
-    OPERAND_IREG = 0x6C,
-    OPERAND_LABEL = 0x6D,
-    OPERAND_ILABEL = 0x6E,
+    // literal numbers
+    OPER_INT8 = 0x60,
+    OPER_INT16 = 0x61,
+    OPER_INT32 = 0x62,
+    OPER_INT64 = 0x63,
+    OPER_UINT8 = 0x64,
+    OPER_UINT16 = 0x65,
+    OPER_UINT32 = 0x66,
+    OPER_UINT64 = 0x67,
+    OPER_FLOAT = 0x68,
+    // registers
+    OPER_REG = 0x69,
+    OPER_IREG = 0x6A, // Indirect
+    OPER_XREG = 0x6B, // indeXed
+    // others
+    OPER_STR = 0x6C,
+    OPER_LABEL = 0x6D,
 
     // section designator
     SEC_NONE = 0x70,
@@ -103,60 +104,111 @@ typedef enum _opcode_t {
     SEC_DATA = 0x72,
 
     MIN_INSTRUCTION = 0xC5,
-    OP_BREAK = 0xC5,
-    OP_LOAD = 0xC6,
-    OP_STOR = 0xC7,
-    OP_PUSH = 0xC8,
-    OP_POP = 0xC9,
-    OP_ADD = 0xCA,
+    // CLASS0
+    // These instructions/operations are likely to change over time.
+    // opcode, operand
+    // anon string operands
+    // call an external function
+    OP_EXTERN = 0xC5,
+    // load an external library
+    OP_LINK = 0xC6,
+
+    // CLASS1
+    // opcode, reg, operand
+    // operand can be:
+    //  literal number (incl float)
+    //  anonymous string
+    //  address of a data symbol
+    //  register
+    //  indirect register
+    //  indexed register
+    OP_LOAD = 0xC7,
+
+    // CLASS2
+    // opcode, operand, reg
+    // operand can be
+    //  a data symbol
+    //  register
+    //  indirect register
+    //  indexed register
+    OP_STOR = 0xC8,
+
+    // CLASS3
+    // opcode, reg, reg, reg
+    // destination, left operand, right operand
+    //      even numbers represent signed operations
+    //      odd numbers represent unsigned
+    OP_ADD = 0xC9,
+    OP_ADDU = 0xCA,
     OP_SUB = 0xCB,
-    OP_MUL = 0xCC,
-    OP_DIV = 0xCD,
-    OP_MOD = 0xCE,
-    OP_ADDU = 0xCF,
-    OP_SUBU = 0xD0,
-    OP_MULU = 0xD1,
-    OP_DIVU = 0xD2,
-    OP_MODU = 0xD3,
-    OP_INC = 0xD4,
-    OP_DEC = 0xD5,
-    OP_INCU = 0xD6,
-    OP_DECU = 0xD7,
-    OP_SHL = 0xD8,
-    OP_SHR = 0xD9,
-    OP_AND = 0xDA,
-    OP_OR = 0xDB,
-    OP_XOR = 0xDC,
-    OP_NOT = 0xDD,
-    OP_EQU = 0xDE,
-    OP_EQUU = 0xDF,
-    OP_NEQ = 0xE0,
-    OP_NEQU = 0xE1,
-    OP_LT = 0xE2,
-    OP_LTU = 0xE3,
-    OP_GT = 0xE4,
-    OP_GTU = 0xE5,
-    OP_LTE = 0xE6,
-    OP_LTEU = 0xE7,
-    OP_GTE = 0xE8,
-    OP_GTEU = 0xE9,
-    OP_JMP = 0xEA,
-    OP_JMPT = 0xEB,
-    OP_JMPF = 0xEC,
-    OP_JMPR = 0xED,
-    OP_JMPRT = 0xEE,
-    OP_JMPRF = 0xEF,
-    OP_CALL = 0xF0,
-    OP_CALLT = 0xF1,
-    OP_CALLF = 0xF2,
-    OP_CALLR = 0xF3,
-    OP_CALLRT = 0xF4,
-    OP_CALLRF = 0xF5,
-    OP_RET = 0xF6,
-    OP_RETT = 0xF7,
-    OP_RETF = 0xF8,
-    OP_EXTERN = 0xF9,
-    OP_LINK = 0xFA,
+    OP_SUBU = 0xCC,
+    OP_MUL = 0xCD,
+    OP_MULU = 0xCE,
+    OP_DIV = 0xCF,
+    OP_DIVU = 0xD0,
+    OP_MOD = 0xD1,
+    OP_MODU = 0xD2,
+    OP_AND = 0xD3,
+    OP_OR = 0xD4,
+    OP_XOR = 0xD5,
+
+    // CLASS4
+    // opcode, reg, reg
+    // left operand, right operand
+    //      even numbers represent signed operations
+    //      odd numbers represent unsigned
+    OP_EQU = 0xD6,
+    OP_EQUU = 0xD7,
+    OP_NEQ = 0xD8,
+    OP_NEQU = 0xD9,
+    OP_LT = 0xDA,
+    OP_LTU = 0xDB,
+    OP_GT = 0xDC,
+    OP_GTU = 0xDD,
+    OP_LTE = 0xDE,
+    OP_LTEU = 0xDF,
+    OP_GTE = 0xE0,
+    OP_GTEU = 0xE1,
+
+    // CLASS5 (one register)
+    // opcode, reg
+    //  relative signed value in a register
+    //      even values are relative reg
+    //      odd values are absolute code label
+    OP_JMP = 0xE2,
+    OP_JMPA = 0xE3,
+    OP_JMPT = 0xE4,
+    OP_JMPAT = 0xE5,
+    OP_JMPF = 0xE6,
+    OP_JMPAF = 0xE7,
+    OP_CALL = 0xE8,
+    OP_CALLA = 0xE9,
+    OP_CALLT = 0xEA,
+    OP_CALLAT = 0xEB,
+    OP_CALLF = 0xEC,
+    OP_CALLAF = 0xED,
+
+    // value in a register
+    OP_PUSH = 0xEE,
+    OP_POP = 0xEF,
+    OP_INC = 0xF0,
+    OP_DEC = 0xF1,
+    OP_INCU = 0xF2,
+    OP_DECU = 0xF3,
+    OP_NOT = 0xF4,
+
+    // CLASS6
+    // opcode, reg, byte
+    OP_SHL = 0xF5,
+    OP_SHR = 0xF6,
+
+    // CLASS7
+    // opcode
+    // no operands
+    OP_BREAK = 0xF7,
+    OP_RET = 0xF8,
+    OP_RETT = 0xF9,
+    OP_RETF = 0xFA,
     OP_CLRT = 0xFB,
     OP_SETT = 0xFC,
     OP_EXIT = 0xFD,
@@ -167,6 +219,11 @@ typedef enum _opcode_t {
 } opcode_t;
 
 #define INSTR_IDX(v) ((v)-MIN_INSTRUCTION)
+#define VALIDATE_INSTR(i) do { \
+        if(INSTR_IDX(i) < MIN_INSTRUCTION || INSTR_IDX(i) > MAX_INSTRUCTION) { \
+            error("invalid instruction at 0x%08X: 0x%02X", i, IP_REG); \
+        } \
+    } while(false)
 const char* opcode_to_str(opcode_t type);
 
 #endif /* _OPCODES_H_ */
